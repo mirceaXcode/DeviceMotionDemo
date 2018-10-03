0b00f850336aaab3c9aa32699a4d49e578e7b942
Mircea Popescu <mirceap@gmail.com>
2018-10-03 16:47:18 -0500

    Initial Commit

---
+++ DeviceMotionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DeviceMotionDemo
+//
+//  Created by Mircea Popescu on 10/3/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DeviceMotionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeviceMotionDemo
+//
+//  Created by Mircea Popescu on 10/3/18.
+//  Copyright © 2018 Mircea Popescu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
